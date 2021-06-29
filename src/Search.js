06cb765a8e1a431b23f5177da324c57047d53cab
pasan1227 <pasanratnayake@gmail.com>
2021-06-29 13:21:46 +0530

    setting up  react context api

--- src/Search.js
+++ src/Search.js
@@ -3,9 +3,13 @@ import { Button } from '@material-ui/core';
 import { Mic, SearchTwoTone } from '@material-ui/icons';
 import './Search.css';
 import { useHistory } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
+import { actionTypes } from './reducer';
 
 
 function Search({ hideButtons = false }) {
+    const [{}, dispatch] = useStateValue("");
+
     const [input, setInput] = useState("");
     const history = useHistory();
 
@@ -13,6 +17,11 @@ function Search({ hideButtons = false }) {
         e.preventDefault();
 
         console.log('You hit search button >>> ', input);
+
+        dispatch({
+            type: actionTypes.SET_SEARCH_TERM,
+            term: input
+        })
 
         history.push('/search')
     };
